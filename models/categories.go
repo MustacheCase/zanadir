18f44579549472954eb9ee98059a24a15642d72e
tomersein <tomersein@gmail.com>
2025-02-25 08:17:23 +0200

    add license compliance rules
    Signed-off-by: tomersein <tomersein@gmail.com>

--- models/categories.go
+++ models/categories.go
@@ -3,11 +3,12 @@ package models
 const (
 	SCA     CategoryTitle = "SCA"
 	Secrets CategoryTitle = "Secrets"
+	License CategoryTitle = "License Compliance"
 	Table   Format        = "table"
 	JSON    Format        = "json"
 )
 
-var CategoryTitles = []CategoryTitle{SCA, Secrets}
+var CategoryTitles = []CategoryTitle{SCA, Secrets, License}
 
 type CategoryTitle string
 

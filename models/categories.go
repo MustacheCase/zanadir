1c86d9a0807d898a24497409e8fd3d8d3b124e94
tomersein <tomersein@gmail.com>
2025-03-07 13:27:13 +0200

    add coverage
    Signed-off-by: tomersein <tomersein@gmail.com>

--- models/categories.go
+++ models/categories.go
@@ -5,11 +5,12 @@ const (
 	Secrets   CategoryTitle = "Secrets"
 	Licenses  CategoryTitle = "Licenses"
 	EndOfLife CategoryTitle = "End Of Life"
+	Coverage  CategoryTitle = "Coverage"
 	Table     Format        = "table"
 	JSON      Format        = "json"
 )
 
-var CategoryTitles = []CategoryTitle{SCA, Secrets, Licenses, EndOfLife}
+var CategoryTitles = []CategoryTitle{SCA, Secrets, Licenses, EndOfLife, Coverage}
 
 type CategoryTitle string
 

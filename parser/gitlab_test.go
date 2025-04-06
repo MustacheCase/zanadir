e9f2536426f04613b07c50ad5addcab81d26d5db
Tomer Seinfeld <tomersein@gmail.com>
2025-04-06 17:54:47 +0300

    add gitlab parser

--- parser/gitlab_test.go
+++ parser/gitlab_test.go
@@ -97,20 +97,3 @@ func TestGitlabMalformedFile(t *testing.T) {
 	assert.Error(t, err, "Expected an error for a malformed file")
 	assert.Nil(t, artifacts, "Expected no artifacts for a malformed file")
 }
-
-func TestGitlabNoFile(t *testing.T) {
-	err := setupGitlabTestDir()
-	assert.NoError(t, err)
-
-	defer teardownTestDir()
-
-	// Remove the .gitlab-ci.yml file
-	testFile := filepath.Join(testDir, ".gitlab-ci.yml")
-	err = os.Remove(testFile)
-	assert.NoError(t, err)
-
-	gp := parser.NewGitlabParser()
-	artifacts, err := gp.Parse(testDir)
-	assert.NoError(t, err)
-	assert.Len(t, artifacts, 0, "Expected no artifacts when file is missing")
-}
